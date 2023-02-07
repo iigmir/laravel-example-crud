15617ed77ea8f64f38a6bd9701c073861d882769
iigmir <roc120j@gmail.com>
2023-02-07 19:20:05 +0800

    init

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/**
+ * @see <https://stackoverflow.com/a/34178797>
+ */
+Route::get("/", function () {
+    return Response()->json(array(
+        "message" => "Hello World!",
+        "todolist" => array(
+            "hello",
+            "World",
+            "API"
+        )
+    ));
+});
